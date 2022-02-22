705b8d826654e254b39c4acba8f33e6b66125cb7
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-02-22 23:58:34 +0100

    planet

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Space
 
-Virtual Space Travel
+Explorer - this is just to hone my astronomy and Rust skills.
+
+If you want to explore space virtually, then you can do so immediately with other projects like [Solar System Scope](https://www.solarsystemscope.com/).
 
 # Objectives
 
-1. Visualize stars
+1. Calculate orbits
+2. Visualize stars
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Space, the final frontier");
+    struct Planet {
+        name: String,
+        distance: f64, // metres
+        period: f64    // seconds
+    }
+    let earth = Planet {
+        name: "Earth".to_string(),
+        distance: 149_597_870_700.0, // astronomical unit
+        period: 365.256 * 24.0 * 60.0 * 60.0
+    };
 }
