8151c19dbdfffe1b2deb35dd08efc9898a069bc4
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-02-23 23:41:13 +0100

    mean_anomaly

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ fn main() {
     struct Planet {
         name: String,
         distance: f64, // metres
-        period: f64    // seconds
+        period: f64, // seconds
+        mean_anomaly: f64 // rad
     }
-    let earth = Planet {
+    let mut earth = Planet {
         name: "Earth".to_string(),
         distance: 149_597_870_700.0, // astronomical unit
-        period: 365.256 * 24.0 * 60.0 * 60.0
+        period: 365.256 * 24.0 * 60.0 * 60.0,
+        mean_anomaly: 0.0
     };
+    for time in 1..17 {
+        earth.mean_anomaly += 0.0625;
+        println!("{}", earth.mean_anomaly);
+    }
 }
